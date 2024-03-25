57bb3224e06645f4d80dd1bba06913f5afb34dcb
Santhosh S <officialuse3012@gmail.com>
2024-03-25 15:52:48 +0530

    backend server completed

--- controllers/clotheController.js
+++ controllers/clotheController.js
@@ -1,22 +1,91 @@
-const getAllClothes = (req, res) => {
-  res.status(200).json({ message: "Get all clothes" });
-};
+const asyncHandler = require("express-async-handler");
+const Clothe = require("../models/clotheModel");
 
-const getClotheById = (req, res) => {
-  res.status(200).json({ message: "Get clothe by id" });
-};
+//@desc get All Clothes
+//@route /api/clothes
+//@access Public
+const getAllClothes = asyncHandler(async (req, res) => {
+  const clothes = await Clothe.find({});
+  if (!clothes) {
+    res.status(500);
+    throw new Error("Internal Server Error");
+  }
+  const page = parseInt(req.query.page) || 0;
+  const perPage = parseInt(req.query.limit) || 10;
 
-const createClothe = (req, res) => {
-  res.status(201).json({ message: "Create clothe" });
-};
+  const start = page * perPage;
+  const end = start + perPage;
 
-const updateClothe = (req, res) => {
-  res.status(200).json({ message: "Update clothe" });
-};
+  const paginatedClothes = clothes.slice(start, end);
 
-const deleteClothe = (req, res) => {
-  res.status(200).json({ message: "Delete clothe" });
-};
+  res.status(200).json({
+    items: paginatedClothes,
+    total: clothes.length,
+    page,
+    perPage,
+    toalPages: Math.ceil(clothes.length / perPage),
+  });
+});
+
+//@desc get Clothe by Id
+//@route /api/clothes/:id
+//@access Public
+const getClotheById = asyncHandler(async (req, res) => {
+  const clothe = await Clothe.findById(req.params.id);
+  if (!clothe) {
+    res.status(404);
+    throw new Error("Clothe not found");
+  }
+  res.status(200).json(clothe);
+});
+
+//@desc Create Clothe
+//@route /api/clothes
+//@access Public
+const createClothe = asyncHandler(async (req, res) => {
+  const { name, image, price, rating } = req.body;
+  if (!name || !image || !price || !rating) {
+    res.status(400);
+    throw new Error("All fields are required");
+  }
+  const clothe = await Clothe.create({
+    name,
+    image,
+    price,
+    rating,
+  });
+  res.status(201).json(clothe);
+});
+
+//@desc Update Clothe
+//@route /api/clothes/:id
+//@access Public
+const updateClothe = asyncHandler(async (req, res) => {
+  const clothe = await Clothe.findById(req.params.id);
+  if (!clothe) {
+    res.status(404);
+    throw new Error("Clothe not found");
+  }
+  const updatedClothe = await Clothe.findByIdAndUpdate(
+    req.params.id,
+    req.body,
+    { new: true }
+  );
+  res.status(200).json(updatedClothe);
+});
+
+//@desc Delete Clothe
+//@route /api/clothes/:id
+//@access Public
+const deleteClothe = asyncHandler(async (req, res) => {
+  const clothe = await Clothe.findById(req.params.id);
+  if (!clothe) {
+    res.status(404);
+    throw new Error("Clothe not found");
+  }
+  const deletedClothe = await Clothe.findByIdAndDelete(req.params.id);
+  res.status(200).json(deletedClothe);
+});
 
 module.exports = {
   getAllClothes,
--- server.js
+++ server.js
@@ -1,7 +1,15 @@
 const express = require("express");
 const dotenv = require("dotenv");
+const cors = require("cors");
 const clotheRouter = require("./routes/clotheRouter");
 const connectDb = require("./config/dbConnection");
+const errorHandler = require("./middleware/errorHandler");
+
+const corsOptions = {
+  origin: "http://localhost:4200",
+  optionsSuccessStatus: 204,
+  methods: "GET, POST, PUT, DELETE",
+};
 
 const app = express();
 dotenv.config();
@@ -11,6 +19,8 @@ const PORT = process.env.PORT || 3000;
 const BASE_URL = process.env.BASE_URL || "/api/clothes";
 
 app.use(express.json());
+app.use(cors(corsOptions));
+app.use(errorHandler);
 app.use(BASE_URL, clotheRouter);
 
 app.listen(PORT, () => {
